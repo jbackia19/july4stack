2130e56e24aa2003cbba951b8569ca5bb5245fe3
jbackia19 <131463490+jbackia19@users.noreply.github.com>
2023-07-04 15:34:49 +0530

    Update App.jsx

--- ui-web-app-reactjs/src/components/App.jsx
+++ ui-web-app-reactjs/src/components/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
 
   const handleApiCall = async (e) => {
     // const url = 'http://localhost:9999/'+e.target.name;
-    let url = 'http://13.232.198.238:32470/'+e.target.name
+    let url = 'http://3.110.182.41:32470/'+e.target.name
 
     displayData.url=url
     let options = {}
